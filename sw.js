b6576a73f53773d1ab32648e381e610a28c73dbc
niloofarjamshidi26 <niloofar.jamshidi26@gmail.com>
2026-05-31 00:45:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Clear old cache and unregister
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+  );
+});
