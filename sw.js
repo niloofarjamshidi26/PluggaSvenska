5f4231bb9d60bcecf8f0a297a6922503863557b8
niloofarjamshidi26 <niloofar.jamshidi26@gmail.com>
2026-05-31 01:21:18 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Clear old cache and unregister
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-  );
-});
